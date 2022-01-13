770e8c37e73a534ed5af7ddd979688473fa1468f
Marina <70626285+marinagriaznova@users.noreply.github.com>
2022-01-13 22:45:59 +0300

    13.01.2022 Марина
    добавлено многоуровневое меню для Расписания и Образцов заявлений

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ bot = telebot.TeleBot(token)  # Объявим бота
 
 @bot.message_handler(commands=['start']) #бот обрабатывает команду /start
 def start_message(message):
+
     markup = telebot.types.InlineKeyboardMarkup() #создаем кнопки
     markup.add(telebot.types.InlineKeyboardButton(text='Расписание на неделю', callback_data='timetable')) # markup - объявляет новую переменную с inline keyboard, а markup.add – создает отдельную кнопку
     markup.add(telebot.types.InlineKeyboardButton(text='Список учителей', callback_data='teachers'))   # text - отвечает за текст на кнопке
@@ -17,18 +18,37 @@ def start_message(message):
 def query_handler(call):
 
     bot.answer_callback_query(callback_query_id=call.id, text='Результат вашего запроса.')  # bot.answer_callback_quer – это всплывающее окно, которое будет показано пользователю после нажатия кнопки
-    answer = ''
+    #answer = ''
     if call.data == 'timetable':  # в call.data будет передано значение, которое мы указывали при создании клавиатуры в параметре callback_data
-        answer = 'придумать как вывести таблицу с расписанием'
+        #answer = 'Выберите нужный день'
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(advert) for advert in ['Понедельник', 'Вторник','Среда','Четверг','Пятница','Суббота']])
+        keyboard.add(*[types.KeyboardButton(advert) for advert in ['/start']])
+        bot.send_message(call.message.chat.id, 'Выберите нужный день.',
+                         reply_markup=keyboard)
     elif call.data == 'teachers':
         answer = 'придумать как вывести таблицу с фио + предмет + почта+номер телефона'
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(advert) for advert in ['/start']])
+        bot.send_message(call.message.chat.id, answer)
+
     elif call.data == 'bells':
         answer = 'придумать как вывести таблицу с расписанием звонков'
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(advert) for advert in ['/start']])
+        bot.send_message(call.message.chat.id, answer)
     elif call.data == 'documents':
-        answer = 'придумать как выыести список образцов в виде файлов'
+        #answer = 'Выберите нужный образец.'
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add(*[types.KeyboardButton(advert) for advert in
+                       ['Освобождение.', 'Самостоятельный ход из школы/ с мероприятия.']])
+        keyboard.add(*[types.KeyboardButton(advert) for advert in ['/start']])
+        bot.send_message(call.message.chat.id, 'Выберите нужный образец.',
+                         reply_markup=keyboard)
 
-    bot.send_message(call.message.chat.id, answer)
+
 
     ''' bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)'''
 
-bot.polling()
+
+bot.polling(none_stop=True)
